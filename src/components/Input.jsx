b24a9f7a7ff93c9a95b583ad0b8a2db5538ad7ad
Drishti Shah <drishtishah2004@gmail.com>
2025-07-05 10:39:30 +0530

    setting up other components

---
+++ src/components/Input.jsx
@@ -0,0 +1,9 @@
+export default function Input({ label, isTextarea, ...props }) {
+    return (
+        <p>
+            <label htmlFor="">{label}</label>
+            {isTextarea ? <textarea  {...props} /> : <input {...props} />}
+
+        </p>
+    )
+}
